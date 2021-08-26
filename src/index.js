a3fd4b02ba345f7333ec9983069145fbca67d72a
anastasiiasher <nastyasher@me.com>
2021-08-26 18:10:45 +0200

    trying to fix everything...
    icons please work, description too!!!

--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
 import "./styles.css";
-let now = new Date();
-let weatherhello = document.querySelector("#today-is");
-let date = now.getDate();
-let year = now.getFullYear();
-let months = [
-  "Januart",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-weatherhello.innerHTML = `${day} ${month} ${date} ${year}`;
+function formatDate(timestamp) {
+  let dateElement = document.querySelector("#time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let now = new Date();
+  let weatherhello = document.querySelector("#today-is");
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let months = [
+    "Januart",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  weatherhello.innerHTML = `${day} ${month} ${date} ${year}`;
+  return `${hours}:${minutes}`;
+}
+const axios = require("axios");
+axios.get(`${apiURL}&appid=${apiKey}`).then(formatDate);
 function s4Wapp(event) {
   event.preventDefault();
   let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
@@ -89,20 +107,4 @@ function searchGeo(event) {
 }
 let submitGeo = document.querySelector("#geo");
 submitGeo.addEventListener("click", searchGeo);
-function formatDate(timestamp) {
-  let dateElement = document.querySelector("#time");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${hours}:${minutes}`;
-}
-const axios = require("axios");
-axios.get(`${apiURL}&appid=${apiKey}`).then(formatDate);
+
