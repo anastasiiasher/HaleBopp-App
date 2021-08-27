5916060187365654e73bb5dc871a2ae3ed699024
anastasiiasher <nastyasher@me.com>
2021-08-27 16:11:17 +0200

    http to https

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#big-icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemp = response.data.main.temp;
