845d7443ba597f7fbeb092c0bc0f70d57dd35f10
anastasiiasher <nastyasher@me.com>
2021-08-28 22:56:00 +0200

    renamed

--- src/index.js
+++ src/index.js
@@ -58,8 +58,7 @@ function formatDay(timestamp) {
 }
 function displayForecast(response) {
   let forecast = response.data.daily;
-  let sevendays = document.querySelector("#forecast");
-  console.log(sevendays);
+  let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (0 < index < 6) {
@@ -82,11 +81,10 @@ function displayForecast(response) {
   });
 
   forecastHTML = forecastHTML + `</div>`;
-  sevendays.innerHTML = forecastHTML;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
