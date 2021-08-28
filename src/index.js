94677ff5c3445c7d914d24233e4e6f8d32bb07c5
anastasiiasher <nastyasher@me.com>
2021-08-28 15:35:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,20 +56,15 @@ function formatDay(timestamp) {
   ];
   return days[day];
 }
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  function displayForecast(response) {
-    let forecast = response.data.daily;
-    let forecastElement = document.querySelector("#forecast");
-    let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-      if (1 < index < 6) {
-        forecastHTML =
-          forecastHTML +
-          `
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (1 < index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
    <div class="col-2">
    <div id="weekday"> ${formatDay(forecastDay.dt)} </div>
    <img id="icons" src="http://openweathermap.org/img/wn/${
@@ -82,13 +77,19 @@ function getForecast(coordinates) {
    </div>
    </div>
   `;
-      }
-    });
+    }
+  });
 
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
-    console.log(forecastElement);
-  }
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastElement);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
