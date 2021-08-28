a1c9ab2d5a6dd1b6f37fd9405ea1558622d6e0ed
anastasiiasher <nastyasher@me.com>
2021-08-28 15:33:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,15 +56,20 @@ function formatDay(timestamp) {
   ];
   return days[day];
 }
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (1 < index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (1 < index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `
    <div class="col-2">
    <div id="weekday"> ${formatDay(forecastDay.dt)} </div>
    <img id="icons" src="http://openweathermap.org/img/wn/${
@@ -77,19 +82,13 @@ function displayForecast(response) {
    </div>
    </div>
   `;
-    }
-  });
+      }
+    });
 
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-  console.log(forecastElement);
-}
-
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastElement);
+  }
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
