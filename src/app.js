eeb49a45da4aec61bf4653c41ffdf3fe78406f85
anastasiiasher <nastyasher@me.com>
2021-08-31 20:48:06 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -93,7 +93,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
@@ -132,7 +132,7 @@ function searchGeo(event) {
     console.log(position.coords.latitude);
     console.log(position.coords.longitude);
     let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
   }
   navigator.geolocation.getCurrentPosition(showPosition);
