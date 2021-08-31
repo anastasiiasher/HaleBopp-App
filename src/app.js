494d610fac8ff2e15f9009128746f1639ddadbb1
anastasiiasher <nastyasher@me.com>
2021-08-31 20:49:12 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -123,7 +123,7 @@ getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "72a6f5c8d3593367d6b1bec5268294b4";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&${units}`;
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayTemperature);
 }
 function searchGeo(event) {
